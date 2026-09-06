300aac89e1b492e20cbd99fb90b886a299dd92cd
Synora <2429663187@qq.com>
2026-09-06 13:12:54 +0800

    发布版本1

--- InGame/HashCheck/HashCheck_GameCores.sql
+++ InGame/HashCheck/HashCheck_GameCores.sql
@@ -1,4 +1,4 @@
 UPDATE GameCores
-SET PackageId = '00000000-7369-4685-ab5f-bf77bc22b54e',
+SET PackageId = '00000000-0609-4185-ab5f-bf77bc22b54e',
     DllPrefix = 'GameCore_XP2_Mz'
 WHERE GameCore = 'Expansion2';
